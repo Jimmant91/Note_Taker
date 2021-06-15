485f2676c84a50d64e152db3794bf1fa7b531160
James Antley <jimmy.antley@gmail.com>
2021-06-15 01:22:55 -0400

    corrected readFileSync variable

---
+++ routes/apiRoute.js
@@ -0,0 +1,3 @@
+// Linking route to data source
+const noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+const fs = require("fs")
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const htmlRoute = require('./routes/htmlRoute')
+const apiRoute = require('./routes/apiRoute')
+const fs = require("fs")
 
+// Setting up Express
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-app.use(express.urlencoded({extended:true}));
 // Using middleware to make folder static
 app.use(express.static('public'));
-
 app.use('/', htmlRoute)
+app.use('/', apiRoute)
 
+// Start server
 app.listen(PORT, ()=> {console.log("LISTENING ON PORT", PORT)});
