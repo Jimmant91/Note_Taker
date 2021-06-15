7ec6b31a04739d0ffa72acce01078f9c60ccd564
James Antley <jimmy.antley@gmail.com>
2021-06-15 01:36:45 -0400

    linked to datasource to write/failing

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,3 +1,26 @@
 // Linking route to data source
-const noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 const fs = require("fs")
+const noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+module.exports = (app) => {
+    // GET notes from db
+    app.get("/api/notes", (req, res) => {
+        res.json(noteData);
+    });
+
+    // POST notes and save to db
+    app.post("/api/notes", (req, res) => {
+        const newNote = req.body;
+        const noteId = (noteData.length).toString();
+            console.log(`Note id`, noteId, `created`);
+
+        newNote.id = noteId;
+        noteData.push(newNote);
+
+        fs.writeFileSync("./db/db.json", JSON.stringify(noteData), (err) => {
+            if (err) throw (err);        
+        });
+
+        res.json(noteData);    
+    });
+};
