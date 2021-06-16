09b34201a96b8f66a1495d118646b66317f25a62
James Antley <jimmy.antley@gmail.com>
2021-06-15 23:08:19 -0400

    nearly completed api route

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -23,4 +23,47 @@ module.exports = (app) => {
 
         res.json(noteData);    
     });
+
+    module.exports = (app) => {
+        // GET notes from db
+           app.get("/api/notes", (req, res) => {
+               res.json(noteData);
+           });
+       
+       // POST notes and save to db
+           app.post("/api/notes", (req, res) => {
+       
+               let newNote = req.body;
+               let noteId = (noteData.length).toString();
+                   console.log(`Note id`, noteId, `created`);
+       
+               newNote.id = noteId;
+               noteData.push(newNote);
+               
+               fs.writeFileSync("./db/db.json", JSON.stringify(noteData), (err) => {
+                   if (err) throw (err);        
+               }); 
+       
+               res.json(noteData);    
+           });
+       
+           // DELETE notes
+           app.delete("/api/notes/:id", (req, res) => {
+       
+               let noteId = req.params.id;
+               let newId = 0;
+                   console.log(`Deleted note ${noteId}`);
+       
+               noteData = noteData.filter(currNote => {
+                  return currNote.id != noteId;
+               });
+               for (currNote of noteData) {
+                   currNote.id = newId.toString();
+                   newId++;
+               }
+               fs.writeFileSync("./db/db.json", JSON.stringify(noteData));
+               res.json(noteData);
+           }); 
+       }
+       
 };
--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 
 // Using middleware to make folder static
 app.use(express.static('public'));
-htmlRoute = require('./routes/htmlRoute')(app)
 apiRoute = require('./routes/apiRoute')(app)
+htmlRoute = require('./routes/htmlRoute')(app)
 
 // Start server
 app.listen(PORT, ()=> {console.log("LISTENING ON PORT", PORT)});
